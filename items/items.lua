d5065c33c449892287ff22c277212bb0a36039f1
LAURIDS0 <66024255+LAURIDS0@users.noreply.github.com>
2025-05-22 13:34:57 +0000

    Ui size and ux update

---
+++ items/items.lua
@@ -0,0 +1,40 @@
+items [
+    {
+        name = "hamer",
+        label = "Hamer",
+        image = "item_hamer",
+        description = "This is a hammer.",
+        type = "weapon",
+        damage = 10,
+        durability = 100,
+        rarity = "common"
+    }
+    {
+        name = "screwdriver",
+        label = "Screwdriver",
+        image = "item_screwdriver",
+        description = "This is a screwdriver.",
+        type = "tool",
+        durability = 50,
+        rarity = "uncommon"
+    },
+    {
+        name = "wrench",
+        label = "Wrench",
+        image = "item_wrench",
+        description = "This is a wrench.",
+        type = "weapon",
+        damage = 5,
+        durability = 85,
+        rarity = "rare"
+    },
+    {
+        name = "saw",
+        label = "Saw",
+        image = "item_saw",
+        description = "This is a saw.",
+        type = "tool",
+        durability = 60,
+        rarity = "common"
+    }
+]
